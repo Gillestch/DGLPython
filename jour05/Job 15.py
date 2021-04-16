6c1453764c0586058c23cdf1ff1155c17a2ec4ee
Gillestch <gilles.tcharbatchian@laplateforme.io>
2021-04-16 17:31:41 +0200

    jour05 job15 marche, enfin j'espère ;-)

--- jour05/Job 15.py
+++ jour05/Job 15.py
@@ -25,10 +25,10 @@ curseur.execute(request)
 """
 
 
-skill_id = input("Entre un id de compétance : ")
-request = ("SELECT student_fk,sum(earned) FROM job_skill,"
-          " WHERE job.id=registration.job_fk"
-          " AND registration.group_id="+group_id+""
+skill_id = input("Entre un id de 'skill' : ")
+request = ("SELECT student_fk,sum(earned) FROM acquiered_skill,job_skill"
+          " WHERE job_skill.id=job_skill_fk"
+          " AND skill_fk="+skill_id+""
           " GROUP BY student_fk"
           )
 #print(request)
@@ -39,7 +39,17 @@ datas = curseur.fetchall()
 
 #print(datas)
 
-print("le job est :",titres[0][0])
+maxEarned = 0
+bestStudent = 0
+for id_student,sumEarned in datas:
+    #print(int(sumEarned))
+    if int(sumEarned)>=maxEarned : 
+        bestStudent = id_student
+        maxEarned = int(sumEarned)
+    #print(id_student,sumEarned)
+print("l'id du student ayant le plus de earned est :",bestStudent)
+
+#print("le job est :",titres[0][0])
 #print ("Le cumul des points gagnés par l'étudiant dont l'id est "+student_id+" :")
 #print (datas[0][0]," | ",datas[0][1])
 #for data in datas : print(data[0],":",data[1]) 
